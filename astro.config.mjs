1fa36be6054e9e6ae595361e160990ba5585ef7d
Mariusso <msteam974@gmail.com>
2026-03-21 10:35:40 +0100

    Deploy: Configuration Netlify et mises à jour

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.autoecolenicolascastres.com',
-  build: {
-    outDir: 'dist'
-  }
+  output: 'static',
+  adapter: netlify()
 });
